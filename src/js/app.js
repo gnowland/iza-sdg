28f08abccf1b524ac5d147e943fa2928b17ce737
Gifford Nowland <hi@giffordnowland.com>
2018-09-29 03:51:26 -0700

    add tabbing ability, add loading animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
 const app = (() => {
   function init() {
-    var sdg = document.getElementById("iza-sdg");
-    console.log(sdg);
+    var svg = document.getElementById("iza-sdg"),
+        sdgs = document.getElementById("sdgs"),
+        sdgn = sdgs.getElementsByTagName("g");
+
+    // add .loaded
+    setTimeout(() => {
+      svg.classList.add("loaded");
+    }, 200);
+
+    // add tabindex to slices
+    for (let i = 0; i < sdgn.length; i++) {
+      const el = sdgn[i];
+      el.setAttribute('tabindex', 3);
+    }
   }
 
   return {
