ab6a1cbef47d9c4ff5fbe35db2c690b49ac46466
Gifford Nowland <hi@giffordnowland.com>
2018-09-28 23:45:28 -0700

    remove cruft from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 import $ from 'jquery';
-import app from './app';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const myConst = 'test ES6 transpiling';
-  app.init(); // testing custom module
-  console.log($, myConst); // testing jQuery and transpiling
+  app.init();
 });
