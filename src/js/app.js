6beee2ce6c869cd2f4618a51d0b4eeebbb856139
Gifford Nowland <hi@giffordnowland.com>
2018-09-30 13:03:35 -0700

    added click, keyboard actions

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,45 @@
 const app = (() => {
+
+  function activate(gp, el, now) {
+    const was = sdgs.getAttribute('data-now');
+    // remove .active from all slice
+    for (let i = 0; i < gp.length; i++) {
+      gp[i].classList.remove('active');
+    }
+    // add .active to focused slice
+    el.classList.add('active');
+
+  }
+
   function init() {
-    var svg = document.getElementById("iza-sdg"),
-        sdgs = document.getElementById("sdgs"),
-        sdgn = sdgs.getElementsByTagName("g");
+    const svg = document.getElementById('iza-sdg');
+    const sdgs = document.getElementById('sdgs');
+    const sdgi = sdgs.getElementsByTagName('g');
 
     // add .loaded
     setTimeout(() => {
-      svg.classList.add("loaded");
+      svg.classList.add('loaded');
+      sdgs.setAttribute('data-now', 1);
     }, 200);
 
     // add tabindex to slices
-    for (let i = 0; i < sdgn.length; i++) {
-      const el = sdgn[i];
-      el.setAttribute('tabindex', 3);
+    for (let i = 0; i < sdgi.length; i++) {
+      const el = sdgi[i];
+      const ix = i+1; // current number
+      el.setAttribute('tabindex', 0);
+      el.setAttribute('focusable', 'true');
+
+      // Focus slice
+      // Click
+      el.addEventListener('click', function () {
+        activate(sdgi, el, ix);
+      });
+      // Enter
+      el.addEventListener('keyup', function (e) {
+        if (e.keyCode === 13) {
+          activate(sdgi, el, ix);
+        }
+      });
     }
   }
 
