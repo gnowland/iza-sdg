88962a907d763953de768c3215c80f1388410c4b
Gifford Nowland <hi@giffordnowland.com>
2018-10-01 03:21:09 -0700

    app scripting
    - import json
    - wait for transition to finish before adding content
    - add data from json to content area (title, color, content)
    - set at-x when slice activated, move transition to css

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,66 @@
+'use strict';
+import data from '../../data.json';
+
 const app = (() => {
 
+  const sdgs = document.getElementById('sdgs');
+
+  // sdgs.addEventListener("transitionend", function (event) {
+  //   if (event.srcElement.id == 'sdgs') {
+  //     sdgs.classList.add('turnt');
+  //   }
+  // }, false);
+  
+  function setContent(now) {
+    const container = document.getElementById('sdg-info-block');
+    const title = document.getElementById('sdg-title');
+    const content = document.getElementById('sdg-content');
+    let slice = now - 1;
+
+    // Set title, color, and content
+    title.innerHTML = data[slice]['title'];
+    content.innerHTML = data[slice]['content'];
+  }
+
   function activate(gp, el, now) {
-    const was = sdgs.getAttribute('data-now');
-    // remove .active from all slice
+    // remove .active from all slices
     for (let i = 0; i < gp.length; i++) {
       gp[i].classList.remove('active');
     }
     // add .active to focused slice
     el.classList.add('active');
+    // remove at-x from sdgs
+    let sdgsCl = sdgs.classList;
+    for (let i = sdgsCl.length; i > 0; i--) {
+      sdgsCl.remove(sdgsCl[0]);
+    }
+    // if (was > now) {
+    //   console.log(was + ', ' + now);
+    // }
+    // add new at-x to sdgs
+    sdgsCl.add('pos', 'at-' + now);
 
-    // Rotate
-    const deg = -1 * (now - 1) * 360 / 17 + 60;
-    sdgs.style.transform = "rotate(" + deg + "deg)";
+    // Set content
+    setContent(now);
+
   }
 
   function init() {
-    const svg = document.getElementById('iza-sdg');
-    const sdgs = document.getElementById('sdgs');
+    const svg  = document.getElementById('iza-sdg');
     const sdgi = sdgs.getElementsByTagName('g');
 
     // add .loaded
     setTimeout(() => {
       svg.classList.add('loaded');
-      sdgs.setAttribute('data-now', 1);
+      sdgs.classList.add('neg', 'at-1');
     }, 200);
 
-    // add tabindex to slices
+    // foreach slice
     for (let i = 0; i < sdgi.length; i++) {
       const el = sdgi[i];
       const ix = i+1; // current number
+
+      // add tabindex, focusable to slices
       el.setAttribute('tabindex', 0);
       el.setAttribute('focusable', 'true');
 
@@ -43,6 +75,7 @@ const app = (() => {
           activate(sdgi, el, ix);
         }
       });
+
     }
   }
 
