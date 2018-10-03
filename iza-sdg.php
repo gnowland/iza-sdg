65aea324cf0d0fa3b59070faf625b71b4e336c86
Gifford Nowland <hi@giffordnowland.com>
2018-10-03 02:55:26 -0700

    implement remaining plugin php code
     - add and enqueue scripts and styles separately (register on init)
    - fix scripts_loaded checking
    - add sdg content to shortcode

--- iza-sdg.php
+++ iza-sdg.php
@@ -31,7 +31,7 @@ if (!defined('ABSPATH')) {
 if ( ! class_exists('IzaSdg') ) {
     class IzaSdg {
         public static $instance;
-        public $scripts_loaded = false;
+        private $scripts_loaded;
 
         // Run plugin
         public static function init() {
@@ -43,14 +43,17 @@ if ( ! class_exists('IzaSdg') ) {
 
         // Initialize plugin
         private function __construct() {
-            // Add textdomain (for translations)
-            add_action( 'plugins_loaded',  array( $this, 'load_textdomain' ) );
+            // Set scripts loaded to false
+            $this->scripts_loaded = false;
+
+            // Load textdomain (for translations)
+            add_action('plugins_loaded', [$this, 'load_textdomain']);
 
             // Add shortcode
-            add_shortcode( 'iza_sdg', array( $this, 'load_shortcode' ) );
+            add_shortcode('iza_sdg', [$this, 'add_shortcode']);
 
-            // Add scripts
-            add_action( 'init', array( $this, 'load_scripts' ), 11 );
+            // Register scripts & styles
+            add_action('init', [$this, 'register_scripts'], 11);
         }
 
         // Load textdomain
@@ -58,28 +61,37 @@ if ( ! class_exists('IzaSdg') ) {
             load_plugin_textdomain( 'iza-sdg', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
         }
 
-        // Load scripts
-        public function load_scripts() {
-            // Skip if scripts are already loaded
-            if ($this->scripts_loded) return;
+        // Register scripts & styles
+        public function register_scripts() {
+            wp_register_script('iza-sdg-js', plugin_dir_url(__FILE__) . 'dist/js/main.bundle.js', [], false, true);
+            wp_register_style('iza-sdg', plugin_dir_url(__FILE__) . 'dist/css/main.min.css', [], false, 'all' );
+        }
 
-            // Load JS
-            wp_enqueue_script('iza-sdg-js', plugin_dir_url(__FILE__) . 'dist/js/main.bundle.js', [], filemtime(plugin_dir_url(__FILE__) . 'dist/js/main.bundle.js'), false);
-            wp_enqueue_style('iza-sdg-css', plugin_dir_url(__FILE__) . 'dist/css/main.min.css', [], filemtime(plugin_dir_url(__FILE__) . 'dist/css/main.min.css'), 'all' );
+        // Enqueue scripts & styles
+        public function add_scripts() {
+            // Skip if already loaded
+            if ($this->scripts_loaded) return;
 
-            // Prevent redundant loading of scripts
+            wp_enqueue_script('iza-sdg-js');
+            wp_enqueue_style('iza-sdg');
+
+            // Set scripts to loaded
             $this->scripts_loaded = true;
         }
+
         /**
-         * Returns the HTML markup for the shortcode.
+         * Returns the HTML markup for the shortcode
          * 
          * @return string HTML markup.
          */
-        public function shortcode() {
-            // Output the script
-            $this->load_scripts();
-            // Return the simple markup
-            return '<h1>Woah, Awesome.</h1>';
+        public function add_shortcode() {
+            // Add the scripts & styles
+            $this->add_scripts();
+
+            // Output the content
+            ob_start();
+            include( dirname ( __FILE__ ) . '/shortcode-content.php' );
+            return ob_get_clean();
         }
     }
 }
