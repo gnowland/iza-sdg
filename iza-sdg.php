b0e5e676d1ea90d77eea11e9f049f5585e282204
Gifford Nowland <hi@giffordnowland.com>
2018-10-02 19:30:06 -0700

    added wordpress plugin files

---
+++ iza-sdg.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * @wordpress-plugin
+ * Plugin Name:         IZA SDG
+ * Plugin URI:          https://github.com/gnowland/iza-sdg
+ * Description:         Sustainable Development Goals interactive graphic for https://sustainability.zinc.org
+ * Version:             1.0.0
+ * Author:              Gifford Nowland
+ * Author URI:          https://github.com/gnowland
+ *
+ * License:             
+ * License URI:         
+ *
+ * GitHub Plugin URI:   gnowland/iza-sdg
+ * GitHub Branch:       master
+ *
+ * Text Domain:         iza-sdg
+ * Domain Path:         /languages
+ *
+ */
+
+// Prevent direct execution
+if (!defined('ABSPATH')) {
+    die;
+};
+
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== IZA SDG ===
+Contributors: gnowland
+Tags: interactive
+Requires at least: 4.9.0
+Tested up to: 4.9.8
+Stable tag: 1.0
+License:
+License URI:
+
+== Description ==
+
+Sustainable Development Goals interactive graphic for https://sustainability.zinc.org
+
+Insert graphic via shortcode: `[iza_sdg]`
+
+== Installation ==
+
+1. Upload plugin to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the "Plugins" menu in WordPress.
+3. Installation done!
+
+== Changelog ==
+
+Commit log is available at https://github.com/gnowland/iza-sdg/commits/master
+
+== Upgrade Notice ==
