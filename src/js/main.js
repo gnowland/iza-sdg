f86d684f55d78a0b43666f73815cd53d8bc8936b
Gifford Nowland <hi@giffordnowland.com>
2019-11-20 13:09:50 -0500

    added smoothscroll-polyfill as a dependency

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 //import $ from 'jquery';
 import app from './app';
+import smoothscroll from "smoothscroll-polyfill";
 
 document.addEventListener('DOMContentLoaded', () => {
+  smoothscroll.polyfill();
   app.init();
 });
